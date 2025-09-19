c867bfab159de3e8366ee17f8cfa209a2efd7378
peace <ade12peace@gmail.com>
2025-09-19 21:19:29 +0100

    resolutions

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ const App = () => {
           />
         </picture>
       </section>
-      <main className="w-[80%] md:w-[50%] bg-white mx-auto mb-10 -mt-30 shadow-[0_0_5px_rgba(0,0,0,0.2)] py-6 px-4 relative z-10 rounded-t-2xl">
+      <main className="w-[80%] lg:w-[50%] bg-white mx-auto mb-10 -mt-30 shadow-[0_0_5px_rgba(0,0,0,0.2)] py-6 px-4 relative z-10 rounded-t-2xl">
         <header className="flex items-center gap-4 mb-5">
-          <img className="w-[10%] sm:w-[5%] md:w-[3%]" src={assets.icon_star} alt="icon star" />
+          <img className="w-[10%] sm:w-[5%] lg:w-[3%]" src={assets.icon_star} alt="icon star" />
           <h1 className="text-4xl font-bold">FAQs</h1>
         </header>
         <FaqAcccrdion />
